cba56dc4698faa2b7c5b32cf7460fea1f13a4577
Franck NOUTSA FOTSO <fotsofranck237@gmail.com>
2023-12-04 13:24:21 +0100

    Version finale

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from question import Question
 from questionnaire import Questionnaire
 
-# --- Création d'un tuple questions contenant toutes nos questions et leurs réponse
-questions = (Question("Quelle est la capitale de la France ?", ("Marseille", "Nice", "Paris", "Nantes", "Lille"), "Paris"),
-             Question("Quelle est la capitale de l'Italie ?", ("Rome", "Venise", "Pise", "Florence"), "Rome"),
-             Question("Quelle est la capitale de la France ?", ("Bafoussam", "Yaounde", "Douala", "Buea"), "Yaounde"),
-             Question("Quelle est la capitale de la Belgique ?", ("Anvers", "Bruxelles", "Bruges", "Liège"), "Bruxelles"))
-
-# --- Création d'un objet Questionnaire ---
-questionnaire = Questionnaire(questions)
+# --- Création d'un objet Questionnaire (contenant toutes nos questions et leurs réponse) ---
+questionnaire = Questionnaire(
+    (
+        Question("Quelle est la capitale de la France ?", ("Marseille", "Nice", "Paris", "Nantes", "Lille"), "Paris"),
+        Question("Quelle est la capitale de la Cameroun ?", ("Bafoussam", "Yaounde", "Douala", "Buea"), "Yaounde"),
+        Question("Quelle est la capitale de l'Italie ?", ("Rome", "Venise", "Pise", "Florence"), "Rome"),
+        Question("Quelle est la capitale de la Belgique ?", ("Anvers", "Bruxelles", "Bruges", "Liège"), "Bruxelles")
+    )
+)
 
 # --- Appelle de la méthode lancer questionnaire ---
 questionnaire.lancer()
@@ -16,7 +17,7 @@ questionnaire.lancer()
 """
 # --- Création des objets Question ---
 q1 = Question("Quelle est la capitale de la France ?", ("Marseille", "Nice", "Paris", "Nantes", "Lille"), "Paris")
-q2 = Question("Quelle est la capitale de la France ?", ("Bafoussam", "Yaounde", "Douala", "Buea"), "Yaounde")
+q2 = Question("Quelle est la capitale de la Cameroun ?", ("Bafoussam", "Yaounde", "Douala", "Buea"), "Yaounde")
 q3 = Question("Quelle est la capitale de l'Italie ?", ("Rome", "Venise", "Pise", "Florence"), "Rome")
 q4 = Question("Quelle est la capitale de la Belgique ?", ("Anvers", "Bruxelles", "Bruges", "Liège"), "Bruxelles")
 
